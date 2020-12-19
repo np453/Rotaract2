e0500c2005c9ea3712b4b01d121808d66693abbd
Devang <acw.dnsp@gmail.com>
2020-12-19 12:11:57 +0000

    minor changes

--- client/src/components/testpage.jsx
+++ client/src/components/testpage.jsx
@@ -4,77 +4,27 @@ import ScrollReveal from 'scrollreveal';
 import axios from 'axios';
 const TestPage = props => {
     const { scrollYProgress } = useViewportScroll()
-    const [content, setContent] = useState("dsjkfhjsdhfjkhds")
     const scaleAnim = useTransform(scrollYProgress, [0, 0.33], [1, 1.5])
     const yPosAnim = useTransform(scrollYProgress, [0, 0.33], [0, -0])
     const yPosAnim2 = useTransform(scrollYProgress, [0.33, 0.67], [-10, 5])
     const yPosAnim3 = useTransform(scrollYProgress, [0.67, 1], [0, 5])
-    const opacity = useTransform(scrollYProgress, [0, 0.33, 0.331], [0.8, 1, 0])
+    const opacity = useTransform(scrollYProgress, [0, 0.33, 0.331], [0.2, 1, 0])
     const opacity2 = useTransform(scrollYProgress, [0.33, 0.67, 0.671], [0, 1, 0])
     const opacity3 = useTransform(scrollYProgress, [0.671, 1, 1], [0,1, 0])
-    const contents = [
-        {
-            contentClass:"content-1",
-            content:"Incubate."
-        },
-        {
-            contentClass:"content-2",
-            content:"Innovate."
-        },
-        {
-            contentClass:"content-3",
-            content:"Involve."
-        },
-        {
-            contentClass:"content-4",
-            content:"Image your goal."
-        },
-        {
-            contentClass:"content-5",
-            content:"motivate."
-        },
-        {
-            contentClass:"content-5",
-            content:"focus."
-        },
-        {
-            contentClass:"content-5",
-            content:"energise."
-        },
-        {
-            contentClass:"content-5",
-            content:"staminize."
-        },
-        {
-            contentClass:"content-5",
-            content:"high spirits."
-        },
-        {
-            contentClass:"content-5",
-            content:"power up."
-        },
-        {
-            contentClass:"content-5",
-            content:"get ready."
-        },
-    ]
-
     useEffect(() => {
         // const {data} = await axios.get('http://localhost:4444/upload/img')
         // setGallery(data)
-        ScrollReveal().reveal(".content", { scale:8, duration:1000 })
-        for(let i=0;i<contents.length;i++) {
-            setTimeout(() => {
-                setContent(contents[i].content)
-                console.log(contents[i].content)
-            }, 110*i)
-        }
+        
     }, []);
     return (
         <div className="container-fluid p-0 d-flex justify-content-center align-items-center" style={{height:"150vh"}}> 
             <motion.div className="" >
-                <motion.h1 style={{ y:yPosAnim, opacity: opacity, scale:scaleAnim}} className="content">{content}</motion.h1>
-            </motion.div>          
+                
+                <motion.h1 style={{ y:yPosAnim, opacity: opacity }} className="head1">This is framer motion animation1</motion.h1>
+                <motion.h1 style={{ y:yPosAnim2, opacity: opacity2,}} className="head2">This is framer motion animation2</motion.h1>
+                <motion.h1 style={{ y:yPosAnim3, opacity: opacity3,}} className="head3">This is framer motion animation3</motion.h1>
+            </motion.div>         
+            
         </div>
     );
 }
